379017e63efa05c03e97a844d8173e6d88240a41
marufmustarmoon <marufmustarmoon@gmail.com>
2024-02-21 19:44:43 +0000

    django cors set up added for local development port 3000

--- myblog/settings.py
+++ myblog/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'silk',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +55,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'silk.middleware.SilkyMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000', 
+    
 ]
 
 ROOT_URLCONF = 'myblog.urls'
