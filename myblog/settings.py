0eb5618a07a6ab8f38cf058bd1abc13854639496
marufmustarmoon <marufmustarmoon@gmail.com>
2024-02-24 06:36:12 +0000

    media storage added

--- myblog/settings.py
+++ myblog/settings.py
@@ -169,9 +169,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'uploads'
+
 
 STORAGES = {
-    # ...
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
--- myblog/urls.py
+++ myblog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 
 urlpatterns = [
     path('admin-dj/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('silk/', include('silk.urls', namespace='silk')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
